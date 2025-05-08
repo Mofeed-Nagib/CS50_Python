b5211523d6ebcab02c0923919c5f0d3f0ef68727
Mofeed Nagib <nagibmofeed@gmail.com>
2025-05-08 00:23:38 -0400

    Finish Problem Set 2 - Coke Machine

---
+++ Problem_Set_2/coke/coke.py
@@ -0,0 +1,10 @@
+amount = 50
+
+while amount > 0:
+    print(f"Amount Due: {amount}")
+    coin = int(input("Insert Coin: "))
+
+    if coin in [5, 10, 25]:
+        amount -= coin
+
+print(f"Change Owed: {-amount}")
