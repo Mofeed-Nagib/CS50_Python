e1479a660e7a20f58208068a5d71f8396bb41410
Mofeed Nagib <nagibmofeed@gmail.com>
2025-05-08 10:11:37 -0400

    Finish Problem Set 2 - Vanity Plates

---
+++ Problem_Set_2/plates/plates.py
@@ -0,0 +1,28 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+    elif not s[0:2].isalpha():
+        return False
+    elif not s[2:].isalnum():
+        return False
+    
+    flag = False
+    for char in s[2:]:
+        if not flag and char == "0":
+            return False
+        elif flag and char.isalpha():
+            return False
+        elif char.isdigit():
+            flag = True
+    return True
+
+
+main()
